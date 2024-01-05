876782e1e53420e0165c8e0f5dce3628a77ecd6e
chenbaicheng <1521085156@qq.com>
2024-01-05 19:37:54 +0800

    更新UI，更新委托格式

--- Source/FastInput/Public/FastInputManager.h
+++ Source/FastInput/Public/FastInputManager.h
@@ -52,7 +52,7 @@ public:
 	bool FISaveJson(FString PropertyName, UClass* Class, UStruct* Struct, UClass* ObjectClass, UDataTable* DT);
 
 public:
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(BlueprintAssignable)
 	FOnFastInputDetect OnFastInputDetect;
 
 public:
